9067b7f55cc1fc4124422eac7e4aad57ea41ad8c
Eric Wittmann <eric.wittmann@gmail.com>
2019-11-21 10:54:20 -0500

    very simple demo for 1.0.x release

---
+++ src/main/java/io/apicurio/registry/demo/ApplicationConstants.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2019 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apicurio.registry.demo;
+
+/**
+ * @author eric.wittmann@gmail.com
+ */
+public class ApplicationConstants {
+    
+    public static final String TOPIC_NAME = "AppTopic";
+    public static final String SUBJECT_NAME = "Greeting";
+    
+    public static final String SCHEMA = "{\"type\":\"record\",\"name\":\"Greeting\",\"fields\":[{\"name\":\"Message\",\"type\":\"string\"},{\"name\":\"Time\",\"type\":\"long\"}]}";
+
+}
--- src/main/java/io/apicurio/registry/demo/Lifecycle.java
+++
@@ -1,9 +0,0 @@
-package io.apicurio.registry.demo;
-
-/**
- * @author Ales Justin
- */
-public interface Lifecycle {
-    void start();
-    void stop();
-}
